3e86b2286734b72af5faa5eaee9f8039fcd92ddf
Rocco Barbini <roccobarbi@gmail.com>
2019-07-02 07:22:49 +0200

    Added an initial implementation for the Stack class.

--- src/com/barbinirocco/datastructures/Stack.java
+++ src/com/barbinirocco/datastructures/Stack.java
@@ -9,6 +9,24 @@ package com.barbinirocco.datastructures;
  * @author rocco barbini (roccobarbi@gmail.com)
  *
  */
-public class Stack {
+public class Stack <T> {
+	
+	private T[] stack;
+	private int maxSize;
+	private int currentSize;
+	
+	/**
+	 * Constructs a stack of the specified size and type. The sample element is only used to
+	 * define the type for the stack: it is not pushed to it.
+	 * 
+	 * @param size The maximum size allowed for the stack.
+	 * @param sample An instance of the type that will be stored in the class.
+	 */
+	@SuppressWarnings("unchecked")
+	public Stack (int size, T sample) {
+		this.maxSize = size;
+		this.currentSize = 0;
+		stack = (T[]) (new Object[size]);
+	}
 
 }
