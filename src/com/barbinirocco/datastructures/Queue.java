312436c747fe33e542982f75c714c723f54643de
Rocco Barbini <roccobarbi@gmail.com>
2019-07-23 08:55:08 +0200

    Queue class (and test) added.

---
+++ src/com/barbinirocco/datastructures/Queue.java
@@ -0,0 +1,49 @@
+/**
+ * 
+ */
+package com.barbinirocco.datastructures;
+
+/**
+ * Implements a queue.
+ * 
+ * @author rocco barbini (roccobarbi@gmail.com)
+ *
+ */
+public class Queue <T> {
+	
+	private T[] queue;
+	private int head, tail,  maxSize, currentSize;
+	
+	/**
+	 * Constructs a queue of the specified size and type. The sample element is only used to
+	 * define the type for the queue: it is not pushed to it.
+	 * 
+	 * @param size The maximum size allowed for the queue.
+	 * @param sample An instance of the type that will be stored in the queue.
+	 */
+	@SuppressWarnings("unchecked")
+	public Queue (int size, T sample) {
+		this.maxSize = size;
+		this.currentSize = 0;
+		this.head = 0;
+		this.tail = 0;
+		queue = (T[]) (new Object[size]);
+	}
+	
+	public int getMaxSize() {
+		return maxSize;
+	}
+	
+	public int getCurrentSize() {
+		return currentSize;
+	}
+	
+	public boolean isEmpty() {
+		return getCurrentSize() == 0; 
+	}
+	
+	public boolean isFull() {
+		return getCurrentSize() == getMaxSize();
+	}
+
+}
---
+++ src/com/barbinirocco/datastructures/unittests/QueueTest.java
@@ -0,0 +1,20 @@
+package com.barbinirocco.datastructures.unittests;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import com.barbinirocco.datastructures.Queue;
+import com.barbinirocco.datastructures.exceptions.OverflowException;
+import com.barbinirocco.datastructures.exceptions.UnderflowException;
+
+/**
+ * @author rocco barbini (roccobarbi@gmail.com)
+ *
+ */
+class QueueTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
