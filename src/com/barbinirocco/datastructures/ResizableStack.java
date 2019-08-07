9729458628e24fd780582bae56d7baa54d5a77fd
Rocco Barbini <roccobarbi@gmail.com>
2019-08-07 06:54:51 +0200

    ResizableStack added and resizing logic written

---
+++ src/com/barbinirocco/datastructures/ResizableStack.java
@@ -0,0 +1,76 @@
+/**
+ * 
+ */
+package com.barbinirocco.datastructures;
+
+import com.barbinirocco.datastructures.exceptions.OverflowException;
+import com.barbinirocco.datastructures.exceptions.UnderflowException;
+import com.barbinirocco.datastructures.interfaces.Stack;
+
+/**
+ * Implements a resizable Stack
+ * 
+ * @author rocco barbini (roccobarbi@gmail.com)
+ * @param <T> the type contained in the stack
+ *
+ */
+public class ResizableStack<T> implements Stack<T> {
+	
+	private T[] stack;
+	private int lowerBound, upperBound, currentSize, maxSize;
+	private static final int minSize = 10;
+	
+	@SuppressWarnings("unchecked")
+	public ResizableStack (int size, T sample) {
+		this.maxSize = size >= minSize ? size : minSize;
+		this.currentSize = 0;
+		this.stack = (T[]) (new Object[size]);
+		calculateBounds();
+	}
+	
+	private void checkResize() {
+		if (currentSize > upperBound) {
+			resizeStack(maxSize * 2);
+		} else if (currentSize < lowerBound) {
+			resizeStack((maxSize * 2) / 3);
+		}
+	}
+	
+	private void resizeStack(int size) {
+		this.maxSize = size >= minSize ? size : minSize;
+		@SuppressWarnings("unchecked")
+		T[] resizedStack = (T[]) (new Object[this.maxSize]);
+		for (int i = 0; i < currentSize; i++) {
+			resizedStack[i] = stack[i];
+		}
+		stack = resizedStack;
+	}
+	
+	private void calculateBounds() {
+		lowerBound = maxSize >= minSize * 2 ? this.maxSize / 2 : -1;
+		upperBound = (this.maxSize * 9) / 10;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return currentSize == 0;
+	}
+
+	@Override
+	public boolean isFull() {
+		return currentSize == maxSize;
+	}
+
+	@Override
+	public void push(T element) throws OverflowException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public T pop() throws UnderflowException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
