e12fb60ceff6ce5db8cb0246f5c373ea6937af16
Rocco Barbini <roccobarbi@gmail.com>
2019-08-08 18:29:20 +0200

    Push benchmarks added

--- src/com/barbinirocco/datastructures/unittests/ResizingStackTest.java
+++ src/com/barbinirocco/datastructures/unittests/ResizingStackTest.java
@@ -94,10 +94,48 @@ class ResizingStackTest {
 			stack.pop();
 			fail("Popping empty stack did not underflow!");
 		} catch (UnderflowException e) {
-			e.printStackTrace();
+			// Passed
 		} catch (Exception e) {
 			e.printStackTrace();
 			fail("Exception while popping!");
+		}
+	}
+	
+	void individualBenchmarkPush(int testSize) throws OverflowException {
+		long timeAccumulator = 0, startTime, minTime = Long.MAX_VALUE, maxTime = Long.MIN_VALUE, curTime;
+		ResizingStack<Integer> stack =  new ResizingStack<Integer>(10, Integer.valueOf(1));
+		for (int i = 0; i < testSize; i++) {
+			startTime = System.nanoTime();
+			stack.push(i);
+			curTime = System.nanoTime() - startTime;
+			timeAccumulator += curTime;
+			minTime = Long.min(minTime, curTime);
+			maxTime = Long.max(maxTime, curTime);
+		}
+		System.out.print(">> ");
+		System.out.print(testSize);
+		System.out.println(" elements pushed <<");
+		System.out.print("Min push time (ns): ");
+		System.out.println(minTime);
+		System.out.print("Average push time (ns): ");
+		System.out.println(timeAccumulator / testSize);
+		System.out.print("Max push time (ns): ");
+		System.out.println(maxTime);
+	}
+	
+	@Test
+	void benchmarkPush() {
+		int testSizes[] = {10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000};
+		try {
+			for (int size: testSizes) {
+				individualBenchmarkPush(size);
+			}
+		} catch (OverflowException e) {
+			e.printStackTrace();
+			fail("Overflow, the stack should resize and never overflow!");
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail("Exception while pushing!");
 		}
 	}
 
