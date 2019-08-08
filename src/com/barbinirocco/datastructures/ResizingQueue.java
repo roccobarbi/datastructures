4c8343aad4fbe51b1ee7356ecccfa693b373fa1c
Rocco Barbini <roccobarbi@gmail.com>
2019-08-08 18:35:02 +0200

    constructor, basic accessors and fields created

--- src/com/barbinirocco/datastructures/ResizingQueue.java
+++ src/com/barbinirocco/datastructures/ResizingQueue.java
@@ -13,17 +13,33 @@ import com.barbinirocco.datastructures.interfaces.Queue;
  *
  */
 public class ResizingQueue<T> implements Queue<T> {
+	
+	private T[] queue;
+	private int head, tail,  maxSize, currentSize, upperBound, lowerBound;
+	private static final int minSize = 10;
+	
+	public ResizingQueue(int size, T sample) {
+		this.maxSize = size >= minSize ? size : minSize;
+		this.currentSize = 0;
+		this.head = 0;
+		this.tail = 0;
+		this.queue = (T[]) (new Object[maxSize]);
+		calculateBounds();
+	}
+	
+	private void calculateBounds() {
+		lowerBound = maxSize >= minSize * 2 ? this.maxSize / 2 : -1;
+		upperBound = (this.maxSize * 9) / 10;
+	}
 
 	@Override
 	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		return false;
+		return currentSize == 0;
 	}
 
 	@Override
 	public boolean isFull() {
-		// TODO Auto-generated method stub
-		return false;
+		return currentSize == maxSize;
 	}
 
 	@Override
