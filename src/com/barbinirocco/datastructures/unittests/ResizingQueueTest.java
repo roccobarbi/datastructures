7be7ba65e6b7df2766bcd34d4b806f7fc887c3d4
Rocco Barbini <roccobarbi@gmail.com>
2019-08-09 06:21:08 +0200

    ResizingQueue test completed

--- src/com/barbinirocco/datastructures/unittests/ResizingQueueTest.java
+++ src/com/barbinirocco/datastructures/unittests/ResizingQueueTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 
 import com.barbinirocco.datastructures.ResizingQueue;
 import com.barbinirocco.datastructures.exceptions.OverflowException;
+import com.barbinirocco.datastructures.exceptions.UnderflowException;
 
 class ResizingQueueTest {
 
@@ -37,11 +38,11 @@ class ResizingQueueTest {
 
 	@Test
 	void testIsFull() {
-		int size = 5;
+		int size = 10;
 		ResizingQueue<Integer> queue =  new ResizingQueue<Integer>(size, Integer.valueOf(1));
 		assertFalse(queue.isFull(), "Newly created queue appears full!");
 		try {
-			for (int i = 0; i < 50; i++)
+			for (int i = 0; i < 10; i++)
 				queue.enqueue(i);
 		} catch (OverflowException e) {
 			e.printStackTrace();
@@ -52,12 +53,55 @@ class ResizingQueueTest {
 
 	@Test
 	void testEnqueue() {
-		fail("Not yet implemented"); // TODO
+		int size = 5;
+		ResizingQueue<Integer> queue =  new ResizingQueue<Integer>(size, Integer.valueOf(1));
+		try {
+			for (int i = 0; i < 50; i++)
+				queue.enqueue(1);
+		} catch (OverflowException e) {
+			e.printStackTrace();
+			fail("Overflow, the queue should resize and never overflow!");
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail("Exception while enqueueing!");
+		}
 	}
 
 	@Test
 	void testDequeue() {
-		fail("Not yet implemented"); // TODO
+		int size = 10, el;
+		ResizingQueue<Integer> queue =  new ResizingQueue<Integer>(size, Integer.valueOf(1));
+		try {
+			for (int i = 0; i < 50; i++)
+				queue.enqueue(i);
+		} catch (OverflowException e) {
+			e.printStackTrace();
+			fail("Overflow, the queue should resize and never overflow!");
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail("Exception while enqueueing!");
+		}
+		try {
+			for (int i = 0; i < 50; i++) {
+				el = queue.dequeue();
+				assertTrue(el == i, "Wrong element dequeued!");
+			}
+		} catch (UnderflowException e) {
+			e.printStackTrace();
+			fail("Underflow before reaching zero!");
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail("Exception while dequeueing!");
+		}
+		try {
+			queue.dequeue();
+			fail("Dequeueing empty queue did not underflow!");
+		} catch (UnderflowException e) {
+			// Passed
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail("Exception while dequeueing!");
+		}
 	}
 
 }
