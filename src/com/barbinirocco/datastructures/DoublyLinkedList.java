11ea1c9851b9192a22ca6746304439249df22065
Rocco Barbini <roccobarbi@gmail.com>
2019-08-09 06:58:00 +0200

    Some basic methods added

--- src/com/barbinirocco/datastructures/DoublyLinkedList.java
+++ src/com/barbinirocco/datastructures/DoublyLinkedList.java
@@ -9,29 +9,30 @@ import com.barbinirocco.datastructures.interfaces.LinkedList;
  * Implements the LinkedList interface using a doubly linked list with sentinels.
  * 
  * @author rocco barbini (roccobarbi@gmail.com)
- * @param <K> the type for keys
- * @param <V> the type for values
+ * @param <K> the type for keys, the actual value is not saved
+ * @param <V> the type for values, the actual value is not saved
  *
  */
 public class DoublyLinkedList<K, V> implements LinkedList<K, V> {
 	
-	private Node nil; // sentinel value that represent both the head and the tail
+	private Node<K, V> nil; // sentinel value that represent both the head and the tail
 	
 	public DoublyLinkedList(K keySample, V valueSample) {
-		nil = new Node(null, null);
-		nil.prev = nil.next = nil;
+		nil = new Node<K, V>(null, null);
+		nil.setPrev(nil.setNext(nil));
 	}
 
 	@Override
 	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		return false;
+		return nil.getNext() == nil;
 	}
 
 	@Override
 	public void insert(K key, V value) {
-		// TODO Auto-generated method stub
-		
+		Node<K, V> node = new Node<K, V>(key, value);
+		node.setPrev(nil.getPrev());
+		node.setNext(nil);
+		nil.setPrev(node);
 	}
 
 	@Override
@@ -46,13 +47,47 @@ public class DoublyLinkedList<K, V> implements LinkedList<K, V> {
 		return null;
 	}
 	
+	@SuppressWarnings("hiding")
 	private class Node<K, V> {
-		private Node prev, next;
+		private Node<K, V> prev, next;
 		private K key;
 		private V value;
 		
 		public Node(K key, V value) {
+			this.setKey(key);
+			this.setValue(value);
+		}
+
+		public Node<K, V> getPrev() {
+			return prev;
+		}
+
+		public void setPrev(Node<K, V> prev) {
+			this.prev = prev;
+		}
+
+		public Node<K, V> getNext() {
+			return next;
+		}
+
+		public Node<K, V> setNext(Node<K, V> next) {
+			this.next = next;
+			return next;
+		}
+
+		public K getKey() {
+			return key;
+		}
+
+		public void setKey(K key) {
 			this.key = key;
+		}
+
+		public V getValue() {
+			return value;
+		}
+
+		public void setValue(V value) {
 			this.value = value;
 		}
 	}
