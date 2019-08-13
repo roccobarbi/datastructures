8e5dd30da72cb6c3752351b5d850b45cfa7931ba
Rocco Barbini <roccobarbi@gmail.com>
2019-08-13 07:01:19 +0200

    HashTable interface added

---
+++ src/com/barbinirocco/datastructures/interfaces/HashTable.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package com.barbinirocco.datastructures.interfaces;
+
+/**
+ * Defines the basic operations for a hash table of key-value pairs.
+ * 
+ * @author rocco barbini (roccobarbi@gmail.com)
+ *
+ */
+public interface HashTable <K, V> {
+	
+	/**
+	 * Inserts a key-value pair into the hash table.
+	 * 
+	 * @param key
+	 * @param value
+	 */
+	public void insert(K key, V value);
+	
+	/**
+	 * Searches for a key-value pair in the hash table.
+	 * 
+	 * @param key
+	 * @return a pointer to the value, or null
+	 */
+	public V search(K key);
+	
+	/**
+	 * Deletes a pair from the hash table.
+	 * 
+	 * @param key
+	 * @return a pointer to the deleted value, or null if none was found.
+	 */
+	public V delete(K key);
+}
