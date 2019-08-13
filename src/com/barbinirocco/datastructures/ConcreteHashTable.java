a6805b4bba92e79eb1fc0c7743ce9681e58d287b
Rocco Barbini <roccobarbi@gmail.com>
2019-08-13 07:08:14 +0200

    ConcreteHashTable added

---
+++ src/com/barbinirocco/datastructures/ConcreteHashTable.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package com.barbinirocco.datastructures;
+
+import com.barbinirocco.datastructures.interfaces.HashTable;
+
+/**
+ * @author roccobarbini
+ * @param <K> the type for the key
+ * @param <V> the type for the value
+ *
+ */
+public class ConcreteHashTable<K, V> implements HashTable<K, V> {
+
+	@Override
+	public void insert(K key, V value) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public V search(K key) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public V delete(K key) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
