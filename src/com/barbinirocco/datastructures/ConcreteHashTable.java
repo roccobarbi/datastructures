03f7822fdaff970c5347bb8f19ab7256c57fb96e
Rocco Barbini <roccobarbi@gmail.com>
2019-08-13 07:26:25 +0200

    Pair inner class added.

--- src/com/barbinirocco/datastructures/ConcreteHashTable.java
+++ src/com/barbinirocco/datastructures/ConcreteHashTable.java
@@ -3,6 +3,7 @@
  */
 package com.barbinirocco.datastructures;
 
+import com.barbinirocco.datastructures.exceptions.NullKeyException;
 import com.barbinirocco.datastructures.interfaces.HashTable;
 
 /**
@@ -29,6 +30,38 @@ public class ConcreteHashTable<K, V> implements HashTable<K, V> {
 	public V delete(K key) {
 		// TODO Auto-generated method stub
 		return null;
+	}
+	
+	private class Pair<K,V> {
+		private K key;
+		private V value;
+		
+		public Pair(K key, V value) throws NullKeyException {
+			if (key == null)
+				throw new NullKeyException();
+			this.key = key;
+			this.value = value;
+		}
+
+		@Override
+		public int hashCode() {
+			return key.hashCode();
+		}
+
+		/**
+		 * @return the key
+		 */
+		public K getKey() {
+			return key;
+		}
+
+		/**
+		 * @return the value
+		 */
+		public V getValue() {
+			return value;
+		}
+		
 	}
 
 }
