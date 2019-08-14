bcf737d21395686e4b95ea2f72d26a99c488e10f
Rocco Barbini <roccobarbi@gmail.com>
2019-08-14 06:43:40 +0200

    table of prime sizes added

--- src/com/barbinirocco/datastructures/ConcreteHashTable.java
+++ src/com/barbinirocco/datastructures/ConcreteHashTable.java
@@ -13,10 +13,14 @@ import com.barbinirocco.datastructures.interfaces.HashTable;
  *
  */
 public class ConcreteHashTable<K, V> implements HashTable<K, V> {
-	
+
 	private static final int minSize = 31;
+	private static final int primeSizes[] = { 31, 61, 127, 257, 547, 1031, 2053,
+			4093, 8191, 16411, 32707, 65579, 131071, 262147, 524287, 1048583, 2097169,
+			4194713, 8388617, 16777259, 33554467, 67108879, 134217757, 268435459, 536870923,
+			1073748019, 2147438987 };
 	private Pair[] table;
-	
+
 	public ConcreteHashTable(K keySample, V valueSample) {
 		this.table = new Pair[minSize];
 	}
@@ -24,7 +28,7 @@ public class ConcreteHashTable<K, V> implements HashTable<K, V> {
 	@Override
 	public void insert(K key, V value) throws NullKeyException {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
@@ -38,11 +42,11 @@ public class ConcreteHashTable<K, V> implements HashTable<K, V> {
 		// TODO Auto-generated method stub
 		return null;
 	}
-	
-	private class Pair<K,V> {
+
+	private class Pair<K, V> {
 		private K key;
 		private V value;
-		
+
 		public Pair(K key, V value) throws NullKeyException {
 			if (key == null)
 				throw new NullKeyException();
@@ -68,7 +72,7 @@ public class ConcreteHashTable<K, V> implements HashTable<K, V> {
 		public V getValue() {
 			return value;
 		}
-		
+
 	}
 
 }
