ece721ce9682a6b6dfd4b632af97f9f07bc0dcb6
Rocco Barbini <roccobarbi@gmail.com>
2019-08-22 17:28:21 +0200

    Added a catch on errors to test maximum capacity for the resizing queue

--- src/com/barbinirocco/datastructures/unittests/BenchmarkTest.java
+++ src/com/barbinirocco/datastructures/unittests/BenchmarkTest.java
@@ -52,18 +52,24 @@ public class BenchmarkTest {
 		long timeAccumulator = 0, startTime, minTime = Long.MAX_VALUE, maxTime = Long.MIN_VALUE, curTime;
 		ResizingQueue<Integer> queue =  new ResizingQueue<Integer>(10, Integer.valueOf(1));
 		for (int i = 0; i < testSize; i++) {
-			startTime = System.nanoTime();
-			queue.enqueue(i);
-			curTime = System.nanoTime() - startTime;
-			timeAccumulator += curTime;
-			minTime = Long.min(minTime, curTime);
-			maxTime = Long.max(maxTime, curTime);
+			try {
+				startTime = System.nanoTime();
+				queue.enqueue(i);
+				curTime = System.nanoTime() - startTime;
+				timeAccumulator += curTime;
+				minTime = Long.min(minTime, curTime);
+				maxTime = Long.max(maxTime, curTime);
+			}   catch (Error e) {
+				System.out.println("Exception encountered: " + e.getMessage());
+				System.out.println("Exception caused by index: " + i);
+				break;
+			}
 		}
 		printResultsSingleTestLine(testSize, timeAccumulator / testSize, minTime, maxTime);
 	}
 	
 	private static void resizingQueueBenchmark() {
-		int testSizes[] = {10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000};
+		int testSizes[] = {10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000, 1000000000};
 		try {
 			System.out.println("\nResizingQueue");
 			printResultsSingleTestHead();
