6224481096cfdb1b0402d9edc3b65e208cfcf5c4
Rocco Barbini <roccobarbi@gmail.com>
2019-08-22 06:44:08 +0200

    ResizingQueue benchmark added

--- src/com/barbinirocco/datastructures/unittests/BenchmarkTest.java
+++ src/com/barbinirocco/datastructures/unittests/BenchmarkTest.java
@@ -7,6 +7,7 @@ import static org.junit.jupiter.api.Assertions.fail;
 
 import org.junit.jupiter.api.Test;
 
+import com.barbinirocco.datastructures.ResizingQueue;
 import com.barbinirocco.datastructures.ResizingStack;
 import com.barbinirocco.datastructures.exceptions.OverflowException;
 
@@ -47,7 +48,34 @@ public class BenchmarkTest {
 				resizingStackSingleSizeBenchmark(size);
 			}
 		} catch (Exception e) {
-			e.printStackTrace();
+			System.out.println("Exception encountered: " + e.getMessage());
+		}
+	}
+	
+	private static void resizingQueueSingleSizeBenchmark(int testSize) throws OverflowException {
+		long timeAccumulator = 0, startTime, minTime = Long.MAX_VALUE, maxTime = Long.MIN_VALUE, curTime;
+		ResizingQueue<Integer> queue =  new ResizingQueue<Integer>(10, Integer.valueOf(1));
+		for (int i = 0; i < testSize; i++) {
+			startTime = System.nanoTime();
+			queue.enqueue(i);
+			curTime = System.nanoTime() - startTime;
+			timeAccumulator += curTime;
+			minTime = Long.min(minTime, curTime);
+			maxTime = Long.max(maxTime, curTime);
+		}
+		printResultsSingleTestLine(testSize, timeAccumulator / testSize, minTime, maxTime);
+	}
+	
+	private static void resizingQueueBenchmark() {
+		int testSizes[] = {10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000};
+		try {
+			System.out.println("\nResizingQueue");
+			printResultsSingleTestHead();
+			for (int size: testSizes) {
+				resizingStackSingleSizeBenchmark(size);
+			}
+		} catch (Exception e) {
+			System.out.println("Exception encountered: " + e.getMessage());
 		}
 	}
 
@@ -56,6 +84,7 @@ public class BenchmarkTest {
 	 */
 	public static void main(String[] args) {
 		BenchmarkTest.resizingStackBenchmark();
+		BenchmarkTest.resizingQueueBenchmark();
 	}
 
 }
